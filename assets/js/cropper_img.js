fb7d0661cc8efb71a76933dd76fd860ce531537d
ajishtmohan <41186016+ajishtmohan@users.noreply.github.com>
2024-05-02 20:03:27 +0530

    update

--- assets/js/cropper_img.js
+++ assets/js/cropper_img.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
   let image = document.querySelector('#pl_sample_image');
   let crop = document.querySelector('#crop');
   let pil_photo = document.querySelector('#pil_photo');
+  let $image_input = document.querySelector('#upload_image');
+  let cancel = document.querySelector('#cancel');
   let cropper;
 
   $('#upload_image').change(function (event) {
@@ -23,18 +25,32 @@ $(document).ready(function () {
     }
 
     function initCropper() {
-      console.log(true);
       cropper = new Cropper(image, {
         aspectRatio: 2.35 / 2.8,
-        viewMode: 3,
-        preview: '.preview',
+        viewMode: 2,
       });
     }
 
     crop.addEventListener('click', function (e) {
-      let croppedImg = cropper.getCroppedCanvas().toDataURL('image/png');
+      let croppedImg = cropper
+        .getCroppedCanvas({ width: 470, height: 560 })
+        .toDataURL('image/jpeg');
       pil_photo.src = croppedImg;
+
       $modal.style.display = 'none';
+      destroyCropper();
     });
+
+    cancel.addEventListener('click', function (e) {
+      $modal.style.display = 'none';
+      destroyCropper();
+    });
+
+    function destroyCropper() {
+      if (image.offsetParent === null) {
+        cropper.destroy();
+        $image_input.value = '';
+      }
+    }
   });
 });
